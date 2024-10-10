5d8304a10f76563c971f09437f2b9208addb134e
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2024-10-10 10:39:42 -0300

    feat: add component ListNavHeader

---
+++ app/_components/list-nav-header.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link"
+import React from "react"
+
+const ListNavHeader = () => {
+  return (
+    <nav className="hidden items-center justify-center gap-6 md:flex">
+      <ul className="flex items-center gap-4">
+        <Link
+          className="text-sm text-white transition-all delay-75 hover:text-yellow-color"
+          href="/"
+        >
+          <li>Home</li>
+        </Link>
+        <Link
+          className="text-sm text-white transition-all delay-75 hover:text-yellow-color"
+          href="/quem-somos"
+        >
+          <li>Quem Somos</li>
+        </Link>
+        <Link
+          className="text-sm text-white transition-all delay-75 hover:text-yellow-color"
+          href="/produtos"
+        >
+          <li>Produtos</li>
+        </Link>
+        <Link
+          className="text-sm text-white transition-all delay-75 hover:text-yellow-color"
+          href="/contato"
+        >
+          <li>Contato</li>
+        </Link>
+      </ul>
+      <a
+        href="#"
+        className="rounded-md bg-yellow-color px-8 py-2 transition-all delay-75 hover:bg-[#D3CB18]"
+      >
+        <button className="text-sm text-black">Quero me inscrever</button>
+      </a>
+    </nav>
+  )
+}
+
+export default ListNavHeader
