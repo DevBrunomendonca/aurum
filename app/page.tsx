2b97f0f6cabab09c7f62ed2eeba2fb8c0a4b96de
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2024-10-11 19:29:59 -0300

    feat: add HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-const Home = () => {
-  return <></>
+import About from "./_components/about"
+import Main from "./_components/main"
+import Team from "./_components/team"
+import VideoInstitutional from "./_components/video-institutional"
+
+const HomePage = () => {
+  return (
+    <>
+      <Main />
+      <Team />
+      <About />
+      <VideoInstitutional />
+    </>
+  )
 }
 
-export default Home
+export default HomePage
