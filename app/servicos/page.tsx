e20c4872492ba820fd3653a65ca4d4df5cecf985
Aurum Capital <aurum.capital.gyn@gmail.com>
2024-10-31 14:52:38 -0300

    Update page.tsx

--- app/servicos/page.tsx
+++ app/servicos/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import CardStage from "./_components/card-stage";
+import Image from "next/image"
+import CardStage from "./_components/card-stage"
 
 const ProductsPage = () => {
   return (
